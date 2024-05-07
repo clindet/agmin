c62e92360a550642c01b36d9a62457d9746ee4cd
张宇亮 <zyl915661305@hotmail.com>
2024-05-07 16:17:48 +0800

    add SenMayo gene set

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' SenMayoGene aging gene set geneid table
+#' @source A new gene set identifies senescent cells and predicts senescence-associated pathways across tissues
+"SenMayoGene"
+
+#' SenMayoName aging gene set gene name table
+#' @source A new gene set identifies senescent cells and predicts senescence-associated pathways across tissues
+"SenMayoName"
+
+#' SenMayoSet raw dataset
+#' @source A new gene set identifies senescent cells and predicts senescence-associated pathways across tissues
+"SenMayoSet"
---
+++ R/score.R
@@ -0,0 +1,37 @@
+
+#' GSEA for SenMayo geneset
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' data(geneList, package="DOSE")
+#' gene <- geneList[abs(geneList) > 2]
+#' res <- SenMayo_GSEA(gene,'Geneid',pvalueCutoff=1, minGSSize =1) # for all result (no-sign result include)
+#' @importFrom clusterProfiler GSEA enricher
+SenMayo_GSEA <- function(geneList,type = 'Geneid',...){
+  if (type == 'Geneid') {
+    res <- GSEA(geneList = geneList,TERM2GENE = SenMayoGene,...)
+  } else if(type == 'Name') {
+    res <- GSEA(geneList = geneList,TERM2NAME = SenMayoName,...)
+  } else {
+    stop('Not support Gene Type')
+  }
+  return(res)
+}
+
+
+
+#' GSVA (orssGSEA) for SenMayo geneset for each cell or sample
+#' obj sample x gene
+#'
+#' @return list
+#' @export
+#' @examples
+#' tpm_sample <- data(tpm_sample)
+#' SenMayo_GSVA(t(tpm_sample),groups = 10, cores = 2)
+#' @importFrom escape enrichIt
+SenMayo_GSVA <- function(obj,...){
+  res <- enrichIt(obj = obj, gene.sets = SenMayoSet,...)
+  return(res)
+}
